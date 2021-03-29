00799c81277d13f9c5299062f838df5ae8e834b1
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-03-29 21:19:49 +0300

    Update dik_ayak_dik_boyu_hesabı.dart

--- dik_ayak_dik_boyu_hesabı.dart
+++ dik_ayak_dik_boyu_hesabı.dart
@@ -8,9 +8,9 @@ import 'dart:math' as Math;
  * 1. Koordinatlardan (AB) ve (AC) semt açıları hesaplanır.
  * 2. (AB) için tan(alfa) = (yb - ya) / (xb - xa). Bölgeye göre (AB) elde edilir.
  * 3. (AC) için tan(beta) = (yc - ya) / (xc - xa). Bölgeye göre (AC) elde edilir. 
- * 4. Büyük semtten küçük semt çıkarılarak A noktasındaki φ açısı hesaplanır. 
+ * 4. Büyük semtten küçük semt çıkarılarak A noktasındaki φ(fi) açısı hesaplanır. 
  * 5. Daha sonra AC uzunluğu AC = sqrt((yc - ya)^2 + (xc - xa)^2) formülünden hesaplanır. 
- * 6. Buluna φ açısı ve AC uzunluğu kullanılarak p dik ayağı ile h dik boyu 
+ * 6. Buluna φ(fi) açısı ve AC uzunluğu kullanılarak p dik ayağı ile h dik boyu 
  * p = AC * cos(φ)
  * h = AC * sin(φ)
  */
@@ -41,7 +41,7 @@ void main()
     // beta semt açısının hesabı
     double beta_radyan = (yc - ya) / (xc - xa);
     //print(beta_radyan);
-    var beta = Math. atan(beta_radyan);
+    var beta = Math.atan(beta_radyan);
     print("beta: $beta");
 }
 
